7ee392ebcf294ba168caf74a8356063c5dc9b6f7
Minh Đức <ducdmd00410@fpt.edu.vn>
2020-05-07 17:43:30 +0700

    file
    fdf

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -299,6 +299,13 @@
             </div>
         </div>
     </section>
+    <div class="position-fixed call">
+        <a href="tel:0345780397" title="Call Now">
+            <div class="quick active"></div>
+            <div class="quick-b active"></div>
+            <div class="quick-a shake"></div>
+        </a>
+    </div>
 </template>
 
 <script>
